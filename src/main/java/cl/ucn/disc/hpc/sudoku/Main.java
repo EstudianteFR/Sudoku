01282e7a678f2e4f45921727d1360f81b6740720
Fabian Rojas <fabian.rojas02@alumnos.ucn.cl>
2021-10-10 05:49:08 -0300

    feat(Sudoku): check box and fill possible options

--- src/main/java/cl/ucn/disc/hpc/sudoku/Main.java
+++ src/main/java/cl/ucn/disc/hpc/sudoku/Main.java
@@ -1,18 +1,81 @@
 package cl.ucn.disc.hpc.sudoku;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+@Slf4j
 public class Main {
 
-    private static int[][] matrix;
+    private static int[][][] matrix;
     private static int n;
     private static int sqrt;
 
     public static void main(String[] args) {
         readFile();
         printMatrix();
+
+        fillOptions();
+    }
+
+    private static void fillOptions() {
+
+        fillBox();
+
+    }
+
+
+    private static void fillBox() {
+        int rowBound = 0;
+        int colBound = 0;
+        int aux = 0;
+
+        // each box
+        for (int i =0; i < n; i++) {
+            if (aux == sqrt){
+                rowBound += sqrt;
+                aux = 0;
+                colBound = 0;
+            }
+
+            int itRow = 0;
+            List<Integer> list = new ArrayList<Integer>(n);
+
+            for (int j = rowBound; itRow < sqrt; j++, itRow++){
+                int itCol = 0;
+                for (int k = colBound; itCol < sqrt; k++, itCol++) {
+                    int value = matrix[j][k][0];
+                    if (value > 0){
+                        list.add(value);
+                    }
+                }
+            }
+
+            itRow = 0;
+            for (int j = rowBound; itRow < sqrt; j++, itRow++){
+                int itCol = 0;
+                for (int k = colBound; itCol < sqrt; k++, itCol++) {
+                    int value = matrix[j][k][0];
+                    if (value == 0){
+                        int iterator = 1;
+                        for (int l = 1; l <= n; l++) {
+                            if (!list.contains(l)){
+                                matrix[j][k][iterator] = l;
+                                iterator++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            aux++;
+            colBound += sqrt;
+        }
+
     }
 
     private static void readFile() {
@@ -22,7 +85,7 @@ public class Main {
             Scanner myReader = new Scanner(myObj);
             n = Integer.parseInt(myReader.nextLine());
             sqrt = (int) Math.sqrt(n);
-            matrix = new int[n][n];
+            matrix = new int[n][n][n+1];
 
             for(int i =0; i < n; i++) {
                 String line = myReader.nextLine();
@@ -33,13 +96,13 @@ public class Main {
                     if (line.charAt(j) != ','){
                         numStr += line.charAt(j);
                     }else{
-                        matrix[i][col] = Integer.parseInt(numStr);
+                        matrix[i][col][0] = Integer.parseInt(numStr);
                         col++;
                         numStr = "";
                     }
                 }
 
-                matrix[i][col] = Integer.parseInt(numStr);
+                matrix[i][col][0] = Integer.parseInt(numStr);
 
             }} catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -50,7 +113,7 @@ public class Main {
     private static void printMatrix() {
         for (int row = 0; row <n; row++){
             for (int col = 0; col < n; col++) {
-                int num = matrix[row][col];
+                int num = matrix[row][col][0];
                 if (num < 10) {
                     System.out.print("0");
                 }
