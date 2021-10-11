f042410b155365a5aac5956f67ca726b0b1a5948
Fabian Rojas <fabian.rojas02@alumnos.ucn.cl>
2021-10-11 05:28:41 -0300

    feat(sudoku): add backtracking algorithm

--- src/main/java/cl/ucn/disc/hpc/sudoku/Main.java
+++ src/main/java/cl/ucn/disc/hpc/sudoku/Main.java
@@ -1,11 +1,11 @@
 package cl.ucn.disc.hpc.sudoku;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class Main {
@@ -31,18 +31,18 @@ public class Main {
 
 
         for (int i = 0; i < 1000; i++) {
-            elimination();
+            //elimination();
 
-            loneRanger();
+            //loneRanger();
 
 
-            twins();
+            //twins();
 
             //triplets();
 
         }
 
-
+        solve(sudoku);
 
         printMatrix();
         percentageSolved();
@@ -955,4 +955,72 @@ public class Main {
         log.debug("The percentage solved is: {}", percentage );
 
     }
+
+    private static boolean solve(int[][][] board) {
+        for (int row = 0; row < n; row++) {
+            for (int column = 0; column < n; column++) {
+                if (board[row][column][0] == 0) {
+                    for (int k = 1; k <= n; k++) {
+                        board[row][column][0] = k;
+                        if (isValid(board, row, column) && solve(board)) {
+                            return true;
+                        }
+                        board[row][column][0] = 0;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][][] board, int row, int column) {
+        return (rowConstraint(board, row)
+                && columnConstraint(board, column)
+                && subsectionConstraint(board, row, column));
+    }
+
+    private static boolean rowConstraint(int[][][] board, int row) {
+        boolean[] constraint = new boolean[n];
+        return IntStream.range(0, n)
+                .allMatch(column -> checkConstraint(board, row, constraint, column));
+    }
+
+    private static boolean columnConstraint(int[][][] board, int column) {
+        boolean[] constraint = new boolean[n];
+        return IntStream.range(0, n)
+                .allMatch(row -> checkConstraint(board, row, constraint, column));
+    }
+
+    private static boolean subsectionConstraint(int[][][] board, int row, int column) {
+        boolean[] constraint = new boolean[n];
+        int subsectionRowStart = (row / sqrt) * sqrt;
+        int subsectionRowEnd = subsectionRowStart + sqrt;
+
+        int subsectionColumnStart = (column / sqrt) * sqrt;
+        int subsectionColumnEnd = subsectionColumnStart + sqrt;
+
+        for (int r = subsectionRowStart; r < subsectionRowEnd; r++) {
+            for (int c = subsectionColumnStart; c < subsectionColumnEnd; c++) {
+                if (!checkConstraint(board, r, constraint, c)) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkConstraint(
+            int[][][] board,
+            int row,
+            boolean[] constraint,
+            int column) {
+        if (board[row][column][0] != 0) {
+            if (!constraint[board[row][column][0] - 1]) {
+                constraint[board[row][column][0] - 1] = true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
